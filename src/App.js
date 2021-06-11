c1dbc520e698a21deae06dde766bb89e5f728e98
Nureeham Abuneepoh <noohannoohan@gmail.com>
2021-06-11 15:40:03 +0700

    edit1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
-import {TextField,Container,Typography} from'@material-ui/core';
+import {TextField,Container,Grid,ButtonBase,Typography,Paper,Tooltip,Button} from'@material-ui/core';
 
    const useStyles = makeStyles((theme) => ({
       root: {
         '& .MuiTextField-root': {
           marginTop: 50,
           width: '50ch',
+       
         }, 
       },
-      iconButton: {
-        marginTop: 50,
-      },
-      Card:{
-        width: '80%',
+      Container:{
+        width: '85%',
         height: 'auto',
         display: 'block',
-        margin: 'auto',
-      },
-      Container:{
-        Text:'center',
+        margin: 'auto',    
+   
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: '100%',
+    background: 'SEASHELL',
+  },
+  image: {
+    width: 700,
+    height:350,
+  },
+  img: {
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
 
-        
-      }
-    }));
+}));
 
 function App() {
   const [data, setData] = useState([]);
   const [search,setSearch] = useState('');
   const [loading,setLoading]= useState(false);
+  const classes = useStyles();
  
   useEffect(() => {
     axios
@@ -46,44 +57,84 @@ function App() {
   if(loading){
     return <p>loading........</p>
   }
-  const filterData = data.filter(data =>{
-    return data.title.includes(search)
+  const mapData = data.map(data => {
+    return data
   })
 
+
   return (
-    <div>
-    <Container >
-      <h1>Travel......</h1>
-      <form>
-    
-      <TextField
+    <Container className={classes.Container} >
+      <h2>{loading}</h2>
+      <h1  style={{textAlign:'center'}} >10 popular tourist attractions from wongnai</h1>
+      <form style={{textAlign:'center'}}>
+        <TextField 
           onChange={(event) => {setSearch(event.target.value)}}
           id="outlined-full-width"
-          style={{ width: '80%' }}
+          style={{ width: 400}}
           placeholder="search"
-          margin="normal"
           variant="outlined"
         />
       </form>
       <br/>
-      {filterData.map((data,idx) => (
+
+      {mapData.map((data,idx) => (
       <DataItem key={idx} {...data} />
       ))}
+
     </Container>
-    
-    </div>
+
   );
 }
-//title,description,photos,tags
-const DataItem = (props) => {
-  const {title} = props;
+//title,description,photos,tags,eid
+const DataItem = ({ title,photos,description,eid,tags})=>{
   const classes = useStyles();
-  return (
-    <Typography className = {classes.Typography}>
-    {title}
-    <br></br>
-    </Typography>
-         
+return (
+    <div className={classes.root}>
+
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Typography gutterBottom variant="subtitle1" >
+                <h2 >{eid}.{title}</h2>
+                
+              <Tooltip >
+                <Button><h3>หมวดหมู่</h3></Button>
+              </Tooltip>
+              <Tooltip >
+                <Button><h4>{tags[0]}</h4></Button>
+              </Tooltip>
+              <Tooltip >
+                <Button><h4>{tags[1]}</h4></Button>
+              </Tooltip>
+              <Tooltip>
+                <Button><h4>{tags[2]}</h4></Button>
+              </Tooltip>
+              <Tooltip >
+                <Button><h4>{tags[3]}</h4></Button>
+            </Tooltip>
+          </Typography>
+        
+      
+          <Typography gutterBottom variant="subtitle1">
+                {description}
+          </Typography>
+            <Grid item>
+              <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={photos[0]} />
+              </ButtonBase>            
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <img className={classes.img} alt="complex" src={photos[1]} style={{width:200,height:120}} />
+                  <img className={classes.img} alt="complex" src={photos[2]} style={{width:200,height:120}}/>  
+                  <img className={classes.img} alt="complex" src={photos[3]} style={{width:200,height:120}}/>
+                </Grid>
+              </Grid>
+            </Grid>
+        </Grid>
+      </Paper>
+      <br></br>
+    </div>
   )
 } 
 
